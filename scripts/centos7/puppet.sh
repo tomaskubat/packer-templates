5028c5b8a99a63b920c15c4eeceeaf32fe1d19ae
Tomas Kubat <tomas.kubat@hotmail.com>
2016-02-05 13:33:19 +0100

    Fix Puppet on version 4.3.1

--- scripts/centos7/puppet.sh
+++ scripts/centos7/puppet.sh
@@ -19,8 +19,8 @@ fi
 # Install Puppet labs repository
 rpm -ivh ${REPO_URL} >/dev/null
 
-# Install Puppet agent package...
-yum install -y puppet-agent
+# Install Puppet agent package: Puppet v4.3.1
+yum install -y puppet-agent-1.3.2-1.el7.x86_64
 
 # Allow call Puppet by sudo
 echo "Defaults secure_path = /sbin:/bin:/usr/sbin:/usr/bin:/opt/puppetlabs/bin" > /etc/sudoers.d/trust-puppetlabs-bin && chmod 440 /etc/sudoers.d/trust-puppetlabs-bin
