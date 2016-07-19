957690638f0e0bedd81bcecb671d957daec369a1
Tomas Kubat <tomas.kubat@hotmail.com>
2016-07-19 20:55:13 +0200

    Upgrade vboxes to Virtualbox 5.0.26

--- scripts/centos7/yum.sh
+++ scripts/centos7/yum.sh
@@ -14,7 +14,8 @@ yum -y remove \
     lvm2-libs
 
 # RPM Forge repository
-rpm -Uvh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
+#rpm -Uvh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
+rpm -Uvh ftp://ftp.pbone.net/mirror/apt.sw.be/redhat/el7/en/x86_64/rpmforge/RPMS/rpmforge-release-0.5.3-1.el7.rf.x86_64.rpm
 
 # Base Packages
 yum -y install \
